e6d1a9711a41ab31ac8e8d40f39823792f6205a1
Iori Suzuki <ioribear@gmail.com>
2024-01-09 23:41:59 +0900

    プロジェクトファイルを追加

---
+++ TimeStampApp/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  TimeStampApp
+//
+//  Created by Iori Suzuki on 2024/01/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    ContentView()
+}
