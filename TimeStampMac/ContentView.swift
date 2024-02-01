df25e40ef313bb8807bfb27d08e41ea68fe41bb4
kirio-1234 <ioribear1228@gmail.com>
2024-02-01 23:03:59 +0900

    Targetを追加

---
+++ TimeStampMac/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TimeStampMac
+//
+//  Created by Iori Suzuki on 2024/02/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TimeStampMac/TimeStampMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeStampMacApp.swift
+//  TimeStampMac
+//
+//  Created by Iori Suzuki on 2024/02/01.
+//
+
+import SwiftUI
+
+@main
+struct TimeStampMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
