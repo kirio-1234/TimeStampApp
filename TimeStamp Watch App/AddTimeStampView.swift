31af55c969e0084d5c9370e58f4a0327933c2799
kirio-1234 <ioribear1228@gmail.com>
2024-05-06 18:11:09 +0900

    fix: 2重にボタンが表示されるのでbuttonStyleをplainに修正

--- TimeStamp Watch App/AddTimeStampView.swift
+++ TimeStamp Watch App/AddTimeStampView.swift
@@ -14,6 +14,7 @@ struct AddTimeStampView: View {
         AddTimeStampButton {
             viewModel.runAction(.edit(timeStamp: .currentTimeStamp))
         }
+        .buttonStyle(.plain)
     }
 }
 
